3354e7196663f36dd58c58bb2c731ad3babe5c79
Melissa Labbe <Melissa.Labbe@mlb.com>
2017-05-24 07:50:22 -0400

    watch GirlsWhoCode and IoT

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const streamIDs = require('./streams/ids');
 
 const streamParameters = {
   follow: streamIDs.getStreamIDs(),
-  track: 'realTrumpVoodoo,NastyWomenCT'
+  track: 'GirlsWhoCode,IoT'
 };
 
 const T = new Twit({
